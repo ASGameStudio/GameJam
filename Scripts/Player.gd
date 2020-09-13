1ea4d5eeeb585d952b3ada9e4238b3440ea44998
The11thJoker <jamessnow98@gmail.com>
2020-09-13 02:01:17 -0500

    Added the movement function

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,5 +1,11 @@
 extends KinematicBody2D
 
+const ACCELERATION = 250
+const MAX_SPEED = 250
+const FRICTION = 750
+
+var velocity = Vector2.ZERO
+
 export(int) var Vital_Energy_Max: int = 100
 var Vital_Energy = Vital_Energy_Max
 var Vital_Energy_Drain = 10
@@ -12,7 +18,21 @@ func _ready():
 	Event.emit_signal("player_energy", Vital_Energy)
 	pass
 
-func _physics_process(_delta):
+#Uses our input map and our constats and variables at the top
+#Creates movement for our player node
+#Make sure to check your input mapping -JS
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+
+	move_and_collide(velocity * delta)
 	#Points the sprite to the Mouse on every Physics Frame.
 	#We should cache the $Sprite in a node, or we could just rotate the entire player instead of their sprite. -AS
 	$Sprite.rotation = get_global_mouse_position().angle()
